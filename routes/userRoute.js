5c2fac015a667d824a5a4691478d2c20fc47f435
Willzi3 <williamszachary787@gmail.com>
2022-07-27 16:32:59 +0200

    update

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,28 +1,80 @@
 const express = require("express");
 const router = express.Router();
 const con = require("../lib/db_connection");
-
 router.get("/", (req, res) => {
-    try {
-        con.query("SELECT * FROM users", (err, result) => {
-            if (err) throw err;
-            res.send(result);
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error)
-    }
+  try {
+    con.query("SELECT * FROM users", (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
 });
+
+//get by id
 router.get("/:id", (req, res) => {
-    try {
-        con.query(`SELECT * FROM users WHERE user_id='${req.params.id}'`, (err, result) => {
-            if (err) throw err;
-            res.send(result);
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error)
-    }
+  try {
+    con.query(
+      `SELECT * FROM users WHERE user_id='${req.params.id}'`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
 });
+
+//post 
+router.post("/", (req, res) => {
+  const {
+    email,
+    password,
+    full_name,
+    billing_address,
+    default_shipping_address,
+    country,
+    phone,
+    user_type,
+  } = req.body;
+  try {
+    con.query(
+      `INSERT INTO users(email,password,full_name,billing_address,default_shipping_address,country,phone,user_type) VALUES ('${email}','${password}','${full_name}','${billing_address}','${default_shipping_address}','${country}','${phone}','${user_type}')`,
+      (err, result)  =>  {
+      if (err) throw err;
+      res.send(result);
+    }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+//put 
+router.put("/:id", (req, res) => {
+    try {
+      con.query(
+        `SELECT * FROM users WHERE user_id='${req.params.id}'`,
+        (err, result) => {
+          if (err) throw err;
+          res.send(result);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+      res.status(400).send(error);
+    }
+  });
+  //delete
 
 module.exports = router;
+
+
+
+
+
+
+
